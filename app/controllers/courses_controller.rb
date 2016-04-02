91d38fb8dd7fe5c02170de1f8f23993988048c58
Elena Caballero <elena.caballero@unitec.edu>
2016-04-02 15:00:10 -0600

    Editing courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -5,6 +5,7 @@ class CoursesController < ApplicationController
   # GET /courses.json
   def index
     @courses = Course.all
+    @course = Course.new
   end
 
   # GET /courses/1
@@ -28,7 +29,7 @@ class CoursesController < ApplicationController
 
     respond_to do |format|
       if @course.save
-        format.html { redirect_to @course, notice: 'Course was successfully created.' }
+        format.html { redirect_to courses_url, notice: 'Course was successfully created.' }
         format.json { render :show, status: :created, location: @course }
       else
         format.html { render :new }
